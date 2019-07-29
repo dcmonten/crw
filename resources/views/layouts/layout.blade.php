3782ab2d21fda8d02a9081cacc28fec37e63a34d
Daniela Montenegro <dcmonten@espol.edu.ec>
2019-07-28 22:33:54 -0500

    ldfj

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -13,7 +13,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link src="{{ asset('css/layout.css') }}" rel="stylesheet" type="text/css">
+    <link href="../css/layout.css" rel="stylesheet" type="text/css">
 
     @yield ('styles')
   </head>
