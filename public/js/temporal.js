00513ec867396011da85cc3535bc4cb585c26c93
Jorge Paladines <jpaladin@espol.edu.ec>
2019-08-07 11:26:17 -0500

    Se muestran los títulos de todos los reportes en una barra superior (de seguro se cambiará luego) y cada reporte con todo su contenido aparece uno debajo de otro. Ahora le meteré un javascript para que sólo se muestre 1 a la vez dependiendo de cuál está seleccionado

--- public/js/temporal.js
+++ public/js/temporal.js
@@ -1,16 +1,17 @@
+//Script para que se muestre la última versión del reporte de manera correcta
 
 /* Se toma el último elemento de la tabla recorriendo los tags: tbody->tr->td, y a eso se le saca el texto puro con tags y todo -->*/
-  var texto_con_tags = $('#final').text();
+var texto_con_tags = $('#final').text();
 
 /*  <!-- Aquí creé un área de texto para meter todo el HTML del reporte -->*/
-  var area = document.createElement('textarea');
+var area = document.createElement('textarea');
 
 /*  <!-- Y entonces a eso le meto el HTML -->*/
-  area.innerHTML = texto_con_tags;
+area.innerHTML = texto_con_tags;
 
 /*  <!-- Tomo el tag <section> y le agrego un título junto con el área de texto que ahora tiene el HTML -->*/
-  var section = $('#final');
+var section = $('#final');
 
-  section.empty();
-  
-  section.append(area.value);
+section.empty();
+
+section.append(area.value);
--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -24,9 +24,12 @@
 </li>
 @endsection
 
-//Esta sección sólo hace que se tome el primer elemento. Después hay que modificar para que se usen varios
+
 @section('que_funcione_solo_1_elemento_del_arreglo')
-{{$collection = $colecciones[0]}}
+@<?php
+  $collection = $colecciones[0]
+
+ ?>
 @endsection
 
 @section('nombre_del_profesor')
@@ -49,59 +52,92 @@
     $collection[X][n] es la fila n del archivo X
     $collection[X][n][m] es el dato de la fila n de la columna m del archivo X
     --}}
-  @php
 
-//ignorando las dos primeras filas, que contienen los headers
+  <?php
+  /*Este arreglo contendrá todos los reportes*/
+  $arreglo_de_reportes = array();
 
-  $aportes = array_slice(json_decode($collection[0], true), 2);
+  //Foreach que se encargará de guardar todo en su lugar correcto
+  foreach ($colecciones as $clave => $collection) {
 
-//seleccionando solamente la última línea
+    //ignorando las dos primeras filas, que contienen los headers
+    $aportes = array_slice(json_decode($collection[0], true), 2);
 
-  $version_final = end($aportes);
-  
-
-//obtener los colaboradores únicos por fuerza bruta
-
-  $colaboradores=array();
-  foreach ($aportes as $key=>$value)
-  {
-    $colaboradores[$key]=$value[0];
-  }
-  $colaboradores=array_unique($colaboradores);
+    //seleccionando solamente la última línea
+    $version_final = end($aportes);
 
 
-//Elimino al primer colabolador, porque es el profesor
-  array_shift($colaboradores);
+    //obtener los colaboradores únicos por fuerza bruta
+    $colaboradores=array();
 
-
-//ahora, con los colaboradores únicos, se almacena cada aporte por colaborador
-
-$aportes_individuales=array();
-
-  foreach ($colaboradores as $persona)
-  {
-    $arreglo_de_aportes=array();
-    foreach ($aportes as $value)
+    foreach ($aportes as $key=>$value)
     {
-      if ($persona==$value[0])
-      {
-        $arreglo_de_aportes[$value[2]]= strip_tags($value[3]);
-      }
-    $aportes_individuales[$persona]=$arreglo_de_aportes;
+      $colaboradores[$key]=$value[0];
     }
-  }
 
-//Ahora se tiene cada usuario como clave de los aportes, y cada fecha como clave del aporte
-
-@endphp
-
-  <section id="pagina" class="row">
+    $colaboradores=array_unique($colaboradores);
 
 
-    <h2 class="col-12 text-center">{{$collection[0][0][0]}}</h2>
+    //Elimino al primer colabolador, porque es el profesor
+    array_shift($colaboradores);
+
+
+    //ahora, con los colaboradores únicos, se almacena cada aporte por colaborador
+    $aportes_individuales=array();
+
+    foreach ($colaboradores as $persona)
+    {
+      $arreglo_de_aportes=array();
+      foreach ($aportes as $value)
+      {
+        if ($persona==$value[0])
+        {
+          $arreglo_de_aportes[$value[2]]= strip_tags($value[3]);
+        }
+      $aportes_individuales[$persona]=$arreglo_de_aportes;
+      }
+    }
+
+    /*Este arreglo contendrá 2 elementos
+    0 - Título del Reporte
+    1 - Aportes individuales ($aportes_individuales)
+    2 - Colaboradores
+    3 - Versión final del reporte ($version_final)
+    */
+    $reporte = array();
+
+    //Se agregan los elementos al reporte
+    array_push($reporte, $collection[0][0][0], $aportes_individuales, $colaboradores, $version_final);
+
+    //Se agrega el reporte a la lista de reportes
+    array_push($arreglo_de_reportes, $reporte);
+
+  }//end foreach exterior
+
+  //Ahora se tiene cada usuario como clave de los aportes, y cada fecha como clave del aporte
+  ?>
+
+  <section id="seleccion_reportes" class="row">
+    @foreach( $arreglo_de_reportes as $numero => $reporte)
+      <h5 id={{'titulo'.$numero}}>{{$reporte[0]}}</h5>
+    @endforeach
+  </section>
+
+<section id="reporte" class="row">
+  @foreach( $arreglo_de_reportes as $numero => $reporte)
+  <section id={{'pagina'.$numero}}} class="row">
+
+    <!--
+    Titulo de la Página: $reporte[0]
+    Aportes Individuales: $reporte[1]
+    Colaboradores: $reporte[2]
+    Versión Final: $reporte[3]
+    -->
+
+    <h2 class="col-12 text-center">{{$reporte[0]}}</h2>
     <h3 class="col-12">Colaboradores: </h3>
     <ul class="col-12">
-      @forelse( $colaboradores as $persona )
+      @forelse( $reporte[2] as $persona )
 
         <li class="col-12">{{$persona}}</li>
 
@@ -111,23 +147,24 @@ $aportes_individuales=array();
 
       @endforelse
     </ul>
+
     <div class="col-lg-6 text-center">
       <h3>Última edición por</h3>
-      <p>{{$version_final[0]}}</p>
+      <p>{{$reporte[3][0]}}</p>
     </div>
     <div class="col-lg-6 text-center">
       <h3>Fecha</h3>
-      <p>{{$version_final[2]}}</p>
+      <p>{{$reporte[3][2]}}</p>
     </div>
 
-    <div id="final">{{$version_final[4]}}</div>
+    <div id="final">{{$reporte[3][4]}}</div>
 
   </section>
 
   <section id="aportes">
     <div id="aporte_por_estudiante">
 
-      @foreach( $aportes_individuales as $persona=>$contribuciones )
+      @foreach( $reporte[1] as $persona=>$contribuciones )
 
         <h3 class="col-6">{{$persona}}</h3>
 
@@ -142,6 +179,9 @@ $aportes_individuales=array();
 
     </div>
   </section>
+@endforeach
+</section>
+
 <section id="charts">
   <div id="heatmap"></div>
   <div id="barchart"></div>
