a7f2f55019388c87f473230169a3b5e4e1c714ef
Jorge Paladines <jpaladin@espol.edu.ec>
2019-08-07 14:01:26 -0500

    Había un error porque sólo el 1er reporte aparecía sin tags. Ya sale bien.

--- public/js/temporal.js
+++ public/js/temporal.js
@@ -1,7 +1,7 @@
 //Script para que se muestre la última versión del reporte de manera correcta
 
 /* Se toma el último elemento de la tabla recorriendo los tags: tbody->tr->td, y a eso se le saca el texto puro con tags y todo -->*/
-var texto_con_tags = $('#final').text();
+var texto_con_tags = $('.reportes_finales').text();
 
 /*  <!-- Aquí creé un área de texto para meter todo el HTML del reporte -->*/
 var area = document.createElement('textarea');
@@ -10,7 +10,7 @@ var area = document.createElement('textarea');
 area.innerHTML = texto_con_tags;
 
 /*  <!-- Tomo el tag <section> y le agrego un título junto con el área de texto que ahora tiene el HTML -->*/
-var section = $('#final');
+var section = $('.reportes_finales');
 
 section.empty();
 
